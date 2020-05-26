59257468bb0a11cb1f5a388c33840c3c8edb0992
podyankee <podyankee@gmail.com>
2020-05-26 13:24:48 +0300

    smooth scrolling to top added

--- source/js/app.js
+++ source/js/app.js
@@ -24,4 +24,48 @@ $(function  () {
 		slidesToScroll: 1,
 		arrows: false
 	});
+
+// Scroll js
+
+	const scrolling = (upSelector) => {
+		const upElem = document.querySelector(upSelector);
+		console.log(upElem);
+
+
+
+		// Scrolling with request animation frame
+
+		let links = document.querySelectorAll('[href^="#"]'),
+			speed = 0.3;
+
+		links.forEach(link => {
+			link.addEventListener('click', function(e) {
+				e.preventDefault();
+				let widthTop = document.documentElement.scrollTop,
+					hash = this.hash,
+					toBlock = document.querySelector(hash).getBoundingClientRect().top,
+					start = null;
+				requestAnimationFrame(step);
+
+				function step(time) {
+					if (start === null) {
+						start = time;
+					}
+					let progress = time - start,
+						r = (toBlock < 0 ? Math.max(widthTop - progress / speed, widthTop + toBlock) : Math.min(widthTop + progress / speed, widthTop + toBlock));
+
+					document.documentElement.scrollTo(0, r);
+
+					if (r != widthTop + toBlock) {
+						requestAnimationFrame(step);
+					} else {
+						location.hash = hash;
+					}
+				}
+			});
+		});
+	};
+
+	scrolling('.up__link');
+
 });
